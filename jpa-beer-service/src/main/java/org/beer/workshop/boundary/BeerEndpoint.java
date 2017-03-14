149a69d78ae704b035deb1fdb21de8d0f37d1892
Alex Soto <asotobu@gmail.com>
2017-03-14 17:46:02 +0100

    Initial commit

---
+++ jpa-beer-service/src/main/java/org/beer/workshop/boundary/BeerEndpoint.java
@@ -0,0 +1,109 @@
+package org.beer.workshop.boundary;
+
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.OptimisticLockException;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.core.UriBuilder;
+
+import org.beer.workshop.entity.Beer;
+
+/**
+ *
+ */
+@Path("/beers")
+public class BeerEndpoint {
+
+    @Inject
+    Beers beers;
+
+    @POST
+    @Consumes("application/json")
+    public Response create(Beer entity) {
+        beers.persist(entity);
+        return Response.created(
+                UriBuilder.fromResource(BeerEndpoint.class)
+                        .path(String.valueOf(entity.getId())).build()).build();
+    }
+
+    @DELETE
+    @Path("/{id:[0-9][0-9]*}")
+    public Response deleteById(@PathParam("id") Long id) {
+        final int numberOfDeletedBeers = beers.remove(id);
+
+        if (numberOfDeletedBeers == 0) {
+            return Response.status(Status.NOT_FOUND).build();
+        }
+        return Response.noContent().build();
+    }
+
+    @GET
+    @Path("/{id:[0-9][0-9]*}")
+    @Produces("application/json")
+    public Response findById(@PathParam("id") Long id) {
+
+        Beer entity;
+        try {
+            entity = beers.findById(id);
+        } catch (NoResultException nre) {
+            entity = null;
+        }
+        if (entity == null) {
+            return Response.status(Status.NOT_FOUND).build();
+        }
+        return Response.ok(entity).build();
+    }
+
+    @GET
+    @Produces("application/json")
+    public List<Beer> listAll(@QueryParam("start") Integer startPosition,
+                              @QueryParam("max") Integer maxResult) {
+        final List<Beer> results = beers.listAll(startPosition, maxResult);
+        return results;
+    }
+
+    @PUT
+    @Path("/{id:[0-9][0-9]*}")
+    @Consumes("application/json")
+    public Response update(@PathParam("id") Long id, Beer entity) {
+        if (entity == null) {
+            return Response.status(Status.BAD_REQUEST).build();
+        }
+        if (id == null) {
+            return Response.status(Status.BAD_REQUEST).build();
+        }
+        if (!id.equals(entity.getId())) {
+            return Response.status(Status.CONFLICT).entity(entity).build();
+        }
+
+        try {
+            entity = beers.update(id, entity);
+
+            if (entity == null) {
+                return Response.status(Status.NOT_FOUND).build();
+            }
+
+        } catch (OptimisticLockException e) {
+            return Response.status(Response.Status.CONFLICT)
+                    .entity(e.getEntity()).build();
+        }
+
+        return Response.noContent().build();
+    }
+}
