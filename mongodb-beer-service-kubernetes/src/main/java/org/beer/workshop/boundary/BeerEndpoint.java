7e7810ba903ff7e89f9460d8210d75c4d00dfd55
Alex Soto <asotobu@gmail.com>
2017-03-27 09:49:29 +0200

    Update Universe to 1.1.13.0 and adds k8s project

---
+++ mongodb-beer-service-kubernetes/src/main/java/org/beer/workshop/boundary/BeerEndpoint.java
@@ -0,0 +1,52 @@
+package org.beer.workshop.boundary;
+
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.OptimisticLockException;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.core.UriBuilder;
+
+import org.beer.workshop.entity.Beer;
+
+/**
+ *
+ */
+@Path("/beers")
+public class BeerEndpoint {
+
+    @Inject
+    Beers beers;
+
+    @POST
+    @Consumes("application/json")
+    public Response create(Beer entity) {
+        beers.persist(entity);
+        return Response.created(
+                UriBuilder.fromResource(BeerEndpoint.class)
+                        .path(String.valueOf(entity.getId())).build()).build();
+    }
+
+    @GET
+    @Produces("application/json")
+    public List<Beer> listAll() {
+        final List<Beer> results = beers.listAll();
+        return results;
+    }
+
+}
---
+++ mongodb-beer-service-kubernetes/src/main/java/org/beer/workshop/boundary/BeerWarehouse.java
@@ -0,0 +1,32 @@
+package org.beer.workshop.boundary;
+
+import redis.clients.jedis.Jedis;
+
+public class BeerWarehouse {
+
+    private Jedis jedis;
+
+    public BeerWarehouse(String host, int port) {
+        this.jedis = new Jedis(host, port);
+    }
+
+    public long increaseStock(String beerId, long numberOfBeers) {
+        return this.jedis.incrBy(beerId, numberOfBeers);
+    }
+
+    public long decreaseStock(String beerId, long numberOfBeers) {
+        return this.jedis.decrBy(beerId, numberOfBeers);
+    }
+
+    public void buyBeer(String beerId, long numberOfBeers) {
+        final long newStock = decreaseStock(beerId, numberOfBeers);
+
+        if (newStock < 0) {
+            // Restore stock
+            increaseStock(beerId, numberOfBeers);
+            throw new IllegalStateException("Not enough beers on the warehouse");
+        }
+
+    }
+
+}
