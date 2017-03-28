0e26b032d73c8767af0a427434d26728b4175ab9
Alex Soto <asotobu@gmail.com>
2017-03-28 18:37:47 +0200

    Adds username/password/database env vars for MongoDB

--- mongodb-beer-service-kubernetes/src/main/java/org/beer/workshop/boundary/MongoProducer.java
+++ mongodb-beer-service-kubernetes/src/main/java/org/beer/workshop/boundary/MongoProducer.java
@@ -2,10 +2,13 @@ package org.beer.workshop.boundary;
 
 
 import com.mongodb.MongoClient;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
 import com.mongodb.client.MongoDatabase;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Produces;
+import java.util.Arrays;
 import java.util.Properties;
 
 @ApplicationScoped
@@ -15,17 +18,28 @@ public class MongoProducer {
     public MongoDatabase createMongoClient() {
         final String host = System.getenv("MONGODB_HOSTNAME");
         final String mongodbPort = System.getenv("MONGODB_PORT");
+        final String username = System.getenv("MONGODB_USER");
+        final String password = System.getenv("MONGODB_PASSWORD");
+        final String database = System.getenv("MONGODB_DATABASE");
+
+        MongoCredential mongoCredential = null;
+
+        if (username != null) {
+            mongoCredential = MongoCredential.createCredential(username,
+                    database,
+                    password.toCharArray());
+
+        }
 
         if (host != null) {
             if (mongodbPort != null) {
-                return new MongoClient(host, Integer.parseInt(mongodbPort)).getDatabase("test");
-            } else {
-
+                ServerAddress serverAddress = new ServerAddress(host, Integer.parseInt(mongodbPort));
+                return new MongoClient(serverAddress, Arrays.asList(mongoCredential)).getDatabase(database == null ? "test" : database);
             }
         }
 
         // Defaults
-        return new MongoClient().getDatabase("test");
+        return new MongoClient().getDatabase(database == null ? "test" : database);
     }
 
 }
